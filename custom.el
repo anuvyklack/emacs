68a0e2fad766d56f87a13f5d2db6a8a51b064406
Yuriy Artemyev <anuvyklack@gmail.com>
2020-09-01 21:58:06 +0300

    Initial commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(org-export-latex-packages-alist (quote ((«» «cmap» t) («english (\, russian») «babel» t))))
+ '(safe-local-variable-values (quote ((evil-shift-width . 2)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
